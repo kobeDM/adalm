360f2e00dc4d185333ba3bcb9be7229f2243503a
Satoshi Higashino <higashino@people.kobe-u.ac.jp>
2021-12-31 19:34:18 +0900

    update bitstream generator

--- adalm_out/source/send_bitstream.cpp
+++ adalm_out/source/send_bitstream.cpp
@@ -33,20 +33,22 @@ bool checkData( vector< unsigned short >* pData );
 int main(int argc, char* argv[])
 {
     printf("send_bitstream: ADALM2000 digital output sample program\n");
-    printf("send_bitstream [-h || -help] [-u || -URI device_URI]\n");
-    int hopt = 0, uopt = 0;
+    printf("send_bitstream [-h || -help] [-u || -URI device_URI] [-f || -file inputfile]\n");
+    int hopt = 0, uopt = 0, fopt = 0;
     double rate=0.0;
     string URI = "";
+    string inputfile = "bitstream.txt";
     char* dparam = nullptr;
     struct option longopts[] = {
         { "URI",  required_argument, nullptr, 'u' },
         { "help", no_argument,       nullptr, 'h' },
+        { "file", required_argument, nullptr, 'f' },
         { 0,      0,                 0,        0  },
     };
     int opt       = 0;
     int longindex = 0;
     int numopt    = 0;
-    while ((opt = getopt_long(argc, argv, "mlhadsu:", longopts, &longindex)) != -1) {
+    while ((opt = getopt_long(argc, argv, "mlhadsuf:", longopts, &longindex)) != -1) {
         switch (opt) {
         case 'h':
             hopt = 1;
@@ -58,6 +60,12 @@ int main(int argc, char* argv[])
             URI=optarg;
             cout <<"URI to access:"<<URI<<endl;
             break;
+        case 'f':
+            fopt=1;
+            numopt+=2;
+            inputfile=optarg;
+            cout <<"Input bitstream file:"<<inputfile<<endl;
+            break;
         default:
             printf("error! \'%c\' \'%c\'\n", opt, optopt);
             return 1;
@@ -66,33 +74,16 @@ int main(int argc, char* argv[])
     //  cout <<"argc"<<argc<<endl;  
     //cout <<"argc"<<argc<<endl;  
     if(hopt){
-        cout <<"ex: ad_out -u usb:1.9.5 --T1 300 --V1 -0.7 --T2 1000 --V2 -1.5 "<<endl;
-        cout <<"\t to output analog signals to CH1 (-0.7V 300ns) and CH2 (-1.5V 1000ns) connected to usb:1.9.5."<<endl;
-        cout <<"ex: ad_out -d usb:1.9.5 --T1 300"<<endl;
-        cout <<"\t to output digital signals to D0:D15 (300ns) connected to usb:1.9.5."<<endl;
-        cout <<"ex: ad_out -s"<<endl;
-        cout <<"\t to get the serial number of available adalm2000."<<endl;
-        cout <<"ex: ad_out -s  -u usb:1.9.5"<<endl;
-        cout <<"\t to get the serial number adalm2000 connected to usb:1.9.5."<<endl;
+        cout <<"ex: send_bitstream -u usb:1.9.5 -f bitstream_user.txt"<<endl;
+        cout <<"\t to output bitstream written in bitstream_user.txt from usb:1.9.5 ADALM2000."<<endl;
+        cout <<"ex: send_bitstream"<<endl;
+        cout <<"\t to output bitstream written in bitstream.txt (default) from ADALM2000 which is found first."<<endl;
         return 0;
     } 
     if(uopt){
         cout<<"URI to access: "<<URI<<endl;
     }
   
-    //----------------------------
-    //  configure
-    /**	const char* fheader = argv[1];
-        int sub_entries = atoi(argv[2]);	
-        double sampling_rate = double(atof(argv[3]));	
-        double sampling_num = atoi(argv[4]);	
-        int dynamic_range = atoi(argv[5]); // V	
-        double Vth1 = double(atof(argv[6])); // V (TRIGGER THRESHOLD)	
-        double Vth2 = double(atof(argv[7])); // V	(TRIGGER THRESHOLD)
-        int trigger_source = atoi(argv[8]);
-        int trig_edge = atoi(argv[9]);
-    **/
-
     //----------------------------
     //  Open ADALM2000
     M2k *ctx;//= m2kOpen();
@@ -131,44 +122,39 @@ int main(int argc, char* argv[])
     std::cout << "##         SEND BITSTREAM          ##" << std::endl;
     std::cout << "#####################################" << std::endl;
     std::cout << std::endl;
+    std::cout << std::endl;
 
-
-    int dT=500; //default 500ns
-    const double dfreq=5e6; // clock for digital
-    int dclocks=((int)(dT*dfreq*1e-9))/4*4;
-    if(dclocks<16)dclocks=16;
-    printf("%dns (%d clocks)\n",dT,dclocks);
+    const double dfreq=1e5; // clock for digital
 
     M2kDigital *dio = ctx->getDigital(); //for dio
     if( dio == nullptr ) return 1;
     dio->setSampleRateOut(dfreq);
-    printf("Digital closk [Hz]: ");
+    printf("Digital clock [Hz]: ");
     printf("%.1e\n", dio->getSampleRateOut());
 	
+    dio->enableAllOut(true);
     for (int i=0;i<16;i++){
         dio->setOutputMode(i,DIO_MODE(1));
         dio->setDirection(i,DIO_DIRECTION(1));
-        //	  dio->enableChannel(i,1);	  
+        dio->enableChannel(i,1);	  
+        dio->setValueRaw(i,DIO_LEVEL(0));
     }
     dio->setCyclic(false);
-    for (int i=0;i<8;i++){
-        dio->setValueRaw(i,DIO_LEVEL(0));
-        dio->setValueRaw(i+8,DIO_LEVEL(0));
-    }
-    dio->enableAllOut(true);
 
     printf("Num of output channel:%d\n",dio->getNbChannelsOut());
 
     vector< unsigned short > dataArr;
-    readBitStream( "bitstream.txt", &dataArr );
+    readBitStream( inputfile, &dataArr );
     cout << dataArr.size( ) << endl;
-    checkData( &dataArr );
+    // checkData( &dataArr );
+    dio->enableAllOut(true);
     dio->push( dataArr );
     
-    sleep( 1 );
+    // sleep( 1 );
 
+    dio->enableAllOut(false);
     contextClose(ctx);
-
+    
     return 0;
 }
 
